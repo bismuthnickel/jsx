d474670e142a39338fc98719d9082ac4b79b33b5
bissy <70106649+gdsnowfluff@users.noreply.github.com>
2023-10-30 13:11:06 -0400

    Add files via upload

---
+++ JSX.js
@@ -0,0 +1,33 @@
+// JSX
+/*
+	Created by Warren Stevens
+	No license yet T-T
+*/
+
+function show(self) {
+	self.style.display = "initial"
+}
+
+function hide(self) {
+	self.style.display = "none"
+}
+
+function pop(self) {
+	self.remove()
+}
+
+function onclick(self,func) {
+	self.addEventListener("click",func)
+}
+
+function onmouseenter(self,func) {
+	self.addEventListener("mouseover",func)
+}
+
+function onmouseexit(self,func) {
+	self.addEventListener("mouseout",func)
+}
+
+function $(_id) {
+	return document.getElementById(_id)
+}
